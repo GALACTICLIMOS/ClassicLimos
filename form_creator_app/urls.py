17977f5ba04f9e824ebd6f1dd32ca0fc95a8a41b
GALACTICLIMOS <144123483+GALACTICLIMOS@users.noreply.github.com>
2023-12-08 18:20:45 +0500

    Add files via upload

--- form_creator_app/urls.py
+++ form_creator_app/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index, name='index'),
+   
+    path('index', views.index, name='index'),
     path('home', views.index, name='index'),
     
     path('reservation', views.Reservation_form, name='reservation'),
